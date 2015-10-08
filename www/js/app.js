f6858037d92837d41b59f62ab34805bd2a5108f2
Ilya Bryliov <bryliov@gmail.com>
2015-10-08 14:13:32 -0700

    sample app using Parse to store values in the database.

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var example = angular.module('starter', ['ionic']);
 
-.run(function($ionicPlatform) {
+example.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,5 +15,42 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    Parse.initialize("MY4KyWo5RUK2yX6GIFEambS54Mv8X4EXm7PIoSBs","qFqeFSzjVHxEpOKYWKjuOHYs42PhkzWWwVSEhaqE");
   });
-})
+});
+
+example.controller("ExampleController", function($scope) {
+ 
+    $scope.savePerson = function(firstname, lastname) {
+        var PeopleObject = Parse.Object.extend("PeopleObject");
+        var person = new PeopleObject();
+        person.set("firstname", firstname);
+        person.set("lastname", lastname);
+        person.save(null, {});
+    }
+
+    $scope.getPeople = function(params) {
+        var PeopleObject = Parse.Object.extend("PeopleObject");
+        var query = new Parse.Query(PeopleObject);
+        if(params !== undefined) {
+            if(params.lastname !== undefined) {
+                query.equalTo("lastname", params.lastname);
+            }
+            if(params.firstname !== undefined) {
+                query.equalTo("firstname", params.lastname);
+            }
+        }
+        query.find({
+            success: function(results) {
+                alert("Successfully retrieved " + results.length + " people!");
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+                    console.log(object.id + ' - ' + object.get("firstname") + " " + object.get("lastname"));
+                }
+            },
+            error: function(error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    };
+  });
