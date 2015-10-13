61f0f65afe3583fdf9108dd5122b81df563d139f
John Wiebe <johnjw59@gmail.com>
2015-10-13 00:12:11 -0700

    camera button now launches the camera and displays a picture that you've taken

--- www/pages/home/home.js
+++ www/pages/home/home.js
@@ -1,4 +1,16 @@
 angular.module('page.home', [])
-  .controller('HomeCtrl', function($scope) {
-    $scope.test = "Hello, world!";
+  .controller('HomeCtrl', function ($scope, $q) {
+    var defer = $q.defer();
+    defer.promise.then(function (img) {
+      $scope.img = img;
+    });
+
+    $scope.takePicture = function () {
+      navigator.camera.getPicture(defer.resolve, defer.reject, {
+        quality: 75,
+        encodingType: Camera.EncodingType.JPEG,
+        correctOrientation: true,
+        saveToPhotoAlbum: false
+      });
+    };
   });
