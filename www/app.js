40a757d7e5c5411527a0e4a509c8f65fb19240dd
pixelvirus <dsongx@gmail.com>
2015-10-17 18:26:31 -0700

    able to login via button and return json. using client id from tripjournalapp@gmail.com

--- www/app.js
+++ www/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ionic', 'page.home'])
+angular.module('app', ['ionic', 'ngCordova', 'page.home'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/pages/home/home.js
+++ www/pages/home/home.js
@@ -1,4 +1,14 @@
 angular.module('page.home', [])
   .controller('HomeCtrl', function($scope) {
     $scope.test = "Hello, world!";
+  })
+
+  .controller("OAuthCtrl", function($scope, $cordovaOauth) {
+      $scope.googleLogin = function() {
+          $cordovaOauth.google("1011840488483-5svsjfnrr9von30eu147lp74qrvbef4p.apps.googleusercontent.com", ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email"]).then(function(result) {
+              console.log(JSON.stringify(result));
+          }, function(error) {
+              console.log(error);
+          });
+      }
   });
