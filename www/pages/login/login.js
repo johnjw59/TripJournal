560ee468ce1c765101ae70c3b83404e1684912b0
John Wiebe <johnjw59@gmail.com>
2015-11-23 00:02:03 -0800

    Fixed case where user didn't have any current trips, adding loading screens

--- www/pages/login/login.js
+++ www/pages/login/login.js
@@ -1,46 +1,59 @@
 angular.module('page.login', [])
-.controller("LoginCtrl", function($scope, $state) {
+.controller("LoginCtrl", function($scope, $state, $ionicLoading) {
   $scope.login = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
+    });
     Parse.User.logIn($scope.email, $scope.password, {
       success: function(user) {
         var userId = user.getUsername();
-        console.log("Log in successful for user " + userId);
 
         var ParseTrip = Parse.Object.extend("Trip");
   
-        //query user's trip ids and check for in-progress trip
+        // query user's trip ids and check for in-progress trip
         var query = new Parse.Query(ParseTrip);
         query.equalTo('userId', userId);
         query.doesNotExist('end');
         query.select('objectId');
         query.first({
           success: function(ret) {
-            window.localStorage.setItem('trip_id', ret.id);
-            $state.go('home');
+            $ionicLoading.hide();
+            if (typeof ret === 'undefined') {
+              $state.go('new-trip');
+            } else {
+              window.localStorage.setItem('trip_id', ret.id);
+              $state.go('home');
+            }
           },
           error: function(err) {
-            $state.go('new-trip');
+            console.error(err);
           }
         });
       },
       error: function(user, error) {
-        console.log("Error logging in with code " + error.code);
+        $ionicLoading.hide();
+        console.error(error);
         alert("Oops: " + error.message + ". Please try again!");
       }
     });
   };
+
   $scope.register = function() {
+    $ionicLoading({
+      template: 'Creating your account...'
+    });
     var user = new Parse.User();
     user.set("username", $scope.email);
     user.set("password", $scope.password);
 
     user.signUp(null, {
       success: function(user) {
-        console.log("Registration success!");
+        $ionicLoading.show();
         $state.go('new-trip');
       },
       error: function(user, error) {
-        console.log("Error registering with code " + error.code);
+        $ionicLoading.hide();
+        console.error(error);
         alert("Oops: " + error.message + ". Please try again!");
       }
     });
