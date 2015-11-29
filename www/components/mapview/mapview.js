d2ef8d33b5b4e6d745082c7ae592d3661035fca8
Ilya Bryliov <bryliov@gmail.com>
2015-11-29 05:03:56 -0800

    Slider's filter properly works and rerenders the markers and the path on the map. Still need to make it work for when viewing older trips

--- www/components/mapview/mapview.js
+++ www/components/mapview/mapview.js
@@ -2,39 +2,52 @@ angular.module('mapview', ['ngMap', 'rzModule'])
 .directive('mapview', function(GeolocationService, $rootScope, $timeout, CardsService) {
   return {
     scope: {
-      markers: '=?markers'
+      markers: '=?markers',
+      cards: '='
     },
     templateUrl: 'components/mapview/mapview.tpl.html',
     controller: function($scope, $state) {
       $scope.detail = function(event, card) {
         $state.go('detail',{card: card});
       };
-
-      $scope.range_slider_ticks_values = {
-        minValue: 1,
-        maxValue: 8,
-        options: {
-          ceil: 10, // change to $scope.endDay
-          floor: 0,
-          showTicksValues: true
-        }
-      };
     },
     link: function($scope) {
+      var MS_PER_DAY = 1000 * 60 * 60 * 24;
+
       var id = window.localStorage.getItem('trip_id');
+
       var ParseTrip = Parse.Object.extend("Trip");
       var query = new Parse.Query(ParseTrip);
+      $scope.trip = null;
+
       query.get(id, {
         success: function(trip) {
-          // uncomment later
-          // $scope.endDay = trip.updatedAt-trip.createdAt;
+          $scope.trip = trip;
+
+          var tripEndDate = trip.updatedAt;
+          if (typeof trip.attributes.end != 'undefined') {
+            tripEndDate = trip.attributes.end;
+          }
+
+          var tripLength = Math.ceil((tripEndDate - trip.attributes.start)/MS_PER_DAY);
+          $scope.slider.options.ceil = tripLength;
+          $scope.slider.options.step = Math.ceil(tripLength/10);
         },
         error: function(object, error) {
           console.log(error);
         }
       });
-      $scope.beginDay = 0;
-      $scope.endDay = 10;
+
+      $scope.slider = {
+        minValue: 1,
+        maxValue: 10,
+        options: {
+          ceil: 10,
+          floor: 1,
+          step: 1,
+          showTicksValues: true
+        }
+      };
 
       var center = null;
 
@@ -49,7 +62,8 @@ angular.module('mapview', ['ngMap', 'rzModule'])
       };
       // If markers aren't provided, use the ones from the service, and center on user
       if (!$scope.hasOwnProperty('markers')) {
-        $scope.markers = CardsService.cards;
+        $scope.cards = CardsService.cards;
+        $scope.markers = $scope.cards;
         center = $scope.userLoc;
       }
 
@@ -60,13 +74,15 @@ angular.module('mapview', ['ngMap', 'rzModule'])
           $scope.center = center;
           console.log($scope);
         }, 250);
+        $scope.cards = $scope.markers; // make a local copy for further filtering
       });      
 
       // Update path when the markers change
       $scope.$watch('markers', function(newVal, oldVal) {
+        // console.log('markers watched', newVal);
         if (typeof newVal != 'undefined') {
-          $scope.path = $scope.markers.map(function(card) {
-            return [card.location.latitude, card.location.longitude];
+          $scope.path = $scope.markers.map(function(marker) {
+            return [marker.location.latitude, marker.location.longitude];
           });
 
           // If center hasn't been set yet, set it to the first marker
@@ -80,7 +96,25 @@ angular.module('mapview', ['ngMap', 'rzModule'])
       }, true);
       
       $rootScope.$on('newCard', function(event, data) {
-        $scope.markers = CardsService.cards;
+        $scope.cards = CardsService.cards;
+        $scope.markers = $scope.cards;
+      });
+
+      $scope.$on("slideEnded", function() {
+     // user finished sliding a handle 
+        $scope.markers = $scope.cards.filter(function(card) {
+          var tripStartDate = $scope.trip.attributes.start;
+          var cardDate = card.createdAt;
+
+          if (Math.ceil((cardDate-tripStartDate)/MS_PER_DAY) >= $scope.slider.minValue &&
+            Math.ceil((cardDate-tripStartDate)/MS_PER_DAY) <= $scope.slider.maxValue) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+        $scope.$apply();
+        // console.log('filtered', $scope.markers);
       });
     }
   };
