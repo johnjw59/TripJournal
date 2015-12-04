2d90f3b5b15d84a8e690601bbf86bf10a6aacc6e
Gisli <gisttor@gmail.com>
2015-12-03 23:46:13 -0800

    Added more test to twitter service

--- www/services/twitterService.spec.js
+++ www/services/twitterService.spec.js
@@ -13,15 +13,50 @@ describe('TwitterService', function() {
   // }));
   
   var TwitterService;
+  var $cordovaOauth;
+  var $twitterApi;
+  var $q;
   
-  beforeEach(function(){
-    module('app');
-    inject(function($injector){
+  beforeEach(inject(function($injector){
       TwitterService = $injector.get('TwitterService');
-    });
-  });
+      $cordovaOauth = $injector.get('$cordovaOauth');
+      $twitterApi = $injector.get('$twitterApi');
+      $q = $injector.get('$q');
+      spyOn($cordovaOauth, 'twitter').and.callFake(function(clientId, clientSecret) {
+        return {
+          then: function(callback, errorCallback) {
+            if (clientId !== 'asdf') {
+              return callback({
+                data: {token: 'some_token'}
+              });
+            } else {
+              return errorCallback('error');
+            }
+          }
+        };
+      });
 
-  describe('Store tokens', function() {
+      spyOn($twitterApi, 'postStatusUpdate').and.callFake(function(message) {
+        return {
+          then: function(callback, errorCallback) {
+            if (message === 'some tweet') {
+              return callback(message);
+            } else {
+              return errorCallback(401);
+            }
+            
+          }
+        };
+      });
+
+      spyOn($twitterApi, 'configure');
+  }));
+
+  afterEach(inject(function() {
+    window.localStorage.clear();
+  }));
+
+  describe('storeUserToken', function() {
     it('stores tokens correctly', function() {
       var token = {token: 'some_token'};
       TwitterService.storeUserToken(token);
@@ -29,7 +64,7 @@ describe('TwitterService', function() {
     });
   });
 
-  describe('Is authenticated', function() {
+  describe('isAuthenticated', function() {
     it('is authenticated if you have token', function() {
       var token = {token: 'some_token'};
       TwitterService.storeUserToken(token);
@@ -37,7 +72,7 @@ describe('TwitterService', function() {
     });
   });
 
-  describe('Initializes', function() {
+  describe('initialize', function() {
     it('initializes correctly if you have token', function() {
       var token = {token: 'some_token'};
       TwitterService.storeUserToken(token);
@@ -45,6 +80,30 @@ describe('TwitterService', function() {
         expect(res.value).toEqual(true);
       });
       TwitterService.configure();
+    });
+  });
+
+  describe('initialize', function() {
+    it('initializes correctly if you dont have a token', function() {
+      TwitterService.initialize().then(function(res) {
+        expect(res.value).toEqual(true);
+      });
+    });
+  });
+
+  describe('postTweet', function() {
+    it('posts tweets', function() {
+      TwitterService.postTweet('some tweet', function(res) {
+        expect(res).toEqual('some tweet');
+      });
+    });
+  });
+
+  describe('postTweet', function() {
+    it('posts tweet error', function() {
+      TwitterService.postTweet('some bad tweet', function(res) {});
+      expect($cordovaOauth.twitter).toHaveBeenCalled();
+      expect($twitterApi.configure).toHaveBeenCalled();
     });
   });
 
